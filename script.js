995c7210ed2eb4674575276804f75ed599f573d1
Jeice Santos <87721461+Jeice01@users.noreply.github.com>
2025-04-11 16:32:23 -0300

    incluido a logica para somar a media mais a AC

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ function calcularMedia() {
     let av2 = parseFloat(document.getElementById("av2").value);
     let avF = parseFloat(document.getElementById("avF").value);
     let avO = parseFloat(document.getElementById("avO").value);
+    let ac  = parseFloat(document.getElementById("ac").value);
 
     let aprovacao = 7;
 
     let media = ((av1 * 2) + (av2 * 2) + (avF * 2) + (avO * 4)) / 10;
+    let notaTotal = media + ac;
 
     let resultado = document.getElementById("resultado");
 
-    if (!isNaN(media)) { // Verifica se a média é um número válido
-        resultado.innerHTML = `A sua média é: ${media.toFixed(2)}`;
+    if (!isNaN(notaTotal)) { // Verifica se a média é um número válido
+        resultado.innerHTML = `A sua média é: ${notaTotal.toFixed(2)}`;
 
-        if (media >= aprovacao) {
+        if (notaTotal >= aprovacao) {
             resultado.innerHTML += "<br>Parabéns, você passou na matéria!!!";
         } else {
-            resultado.innerHTML += `<br>Faltam ${(aprovacao - media).toFixed(2)} pontos para passar!`;
+            resultado.innerHTML += `<br>Faltam ${(aprovacao - notaTotal).toFixed(2)} pontos para passar!`;
         }
     } else {
         resultado.innerHTML = "Por favor, insira notas válidas para calcular a média.";
@@ -31,6 +33,7 @@ function limpar() {
     document.getElementById("av2").value = "";
     document.getElementById("avF").value = "";
     document.getElementById("avO").value = "";
+    document.getElementById("ac").value = "";
     document.getElementById("resultado").innerHTML = "";
   }
 
